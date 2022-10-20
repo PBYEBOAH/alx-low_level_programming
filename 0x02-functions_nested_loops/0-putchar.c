fb9fbd6b62c446f8424b693b9a09ae73ed839452
PBYEBOAH <pbyeboah@gmail.com>
2022-10-19 22:48:38 -0700

    tasks 2 and 3

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char name = 'r';
+
+	printf("_putcha%c\n", name);
+	return (0);
+}
