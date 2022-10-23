1a3a26d80b3855e75c5682db01889ed399016b27
PBYEBOAH <pbyeboah@gmail.com>
2022-10-22 22:45:32 -0700

    tasks 5 - 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * This program will print all single digits from 0 - 9 separated by commas
+ * Only putchar() can be used
+ */
+int main(void)
+{
+	int numz;
+
+	for (numz = 0; numz < 10; numz++)
+	{
+		putchar(numz + '0');
+		if (numz < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
