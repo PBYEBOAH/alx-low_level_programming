08f32965240dcc16833d1f9f49f50fd96bedd27a
PBYEBOAH <pbyeboah@gmail.com>
2022-10-25 22:47:51 -0700

    fix for 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,17 +3,26 @@
 /**
  * print_rev - Prints a string in reverse followed by new line
  * @s: The string to be printed.
+ * @rev - variable to store reversed characters
+ * Return: reversed string
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	char rev[1000];
+	int index, j, count = 0;
 
-	while (s[index++])
-		len++;
+	while (s[count] != '\0') /* finding length of string */
+	{
+		count++;
+	}
+	j = count - 1;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	/* reversing string */
+	for (index = 0; index < count; index++)
+	{
+		rev[index] = s[j];
+		j--;
+		_putchar(rev[index]);
+	}
 	_putchar('\n');
 }
-
